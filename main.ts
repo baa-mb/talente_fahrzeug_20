81cc98787d17107ca3595434ff909d7c9fe71bc1
baa <34665120+baa-mb@users.noreply.github.com>
2026-05-13 18:29:43 +0200

    Finale 1

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     lauf_flag = 1
 })
-serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-	
-})
 // if (info == "TB") {
 // if (wert == 1) {
 // control.raiseEvent(
@@ -18,53 +15,44 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
 // }
 // }
 radio.onReceivedValue(function (info, wert) {
+    serial.writeLine("gerade: " + gerade_get + " kurve: " + kurve_get)
     if (info == "kurve") {
         kurve_get = wert
-        kurve_rad = Math.map(kurve_get, -45, 45, -255, 255)
+        kurve_rad = Math.round(Math.map(kurve_get, -45, 45, -255, 255))
         // basic.showNumber(kurve_rad)
-        kurve_Left = kurve_rad
-        kurve_Right = kurve_rad * -1
-    }
-    if (info == "gerade") {
+        kurve_links = kurve_rad
+        kurve_rechts = kurve_rad * -1
+    } else if (info == "gerade") {
         gerade_get = wert
-        gerade_rad = Math.map(gerade_get, -45, 45, -255, 255)
-        geradeLeft = gerade_rad
-        geradeRight = gerade_rad
-        leftOutput = (geradeLeft + kurve_Left) / 2
-        rightOutput = (geradeRight + kurve_Right) / 2
+        gerade_rad = Math.round(Math.map(gerade_get, -45, 45, -255, 255))
+        gerade_links = gerade_rad
+        gerade_rechts = gerade_rad
+        links_rad = gerade_links + kurve_links
+        rechts_rad = gerade_rechts + kurve_rechts
         if (gerade_get == 0 && kurve_get == 0) {
+            serial.writeLine("----------- stopp ----------------")
             robotbit.MotorStopAll()
         } else {
-            if (leftOutput < 0) {
-                robotbit.MotorRun(motor_links, leftOutput)
-            }
-            if (rightOutput < 0) {
-                robotbit.MotorRun(motor_rechts, rightOutput)
-            }
+            // serial.writeValue("links_rad", links_rad)
+            // serial.writeValue("rechts_rad", rechts_rad)
+            robotbit.MotorRun(motor_links, links_rad)
+            robotbit.MotorRun(motor_rechts, rechts_rad)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     lauf_flag = 0
 })
-let rightOutput = 0
-let leftOutput = 0
-let geradeRight = 0
-let geradeLeft = 0
-let gerade_rad = 0
-let gerade_get = 0
-let kurve_Right = 0
-let kurve_Left = 0
-let kurve_rad = 0
-let kurve_get = 0
+
+let gerade_get = 0, gerade_rad = 0, gerade_rechts = 0, gerade_links = 0
+let kurve_get = 0, kurve_rad = 0, kurve_rechts = 0, kurve_links = 0
+let links_rad = 0, rechts_rad = 0
 let lauf_flag = 0
 radio.setGroup(26)
 lauf_flag = 0
 basic.showIcon(IconNames.Diamond)
-
 let motor_links = robotbit.Motors.M1A
 let motor_rechts = robotbit.Motors.M2B
-
 robotbit.MotorStopAll()
-robotbit.MotorRun(motor_rechts, 70)
-robotbit.MotorRun(motor_links, 70)
+robotbit.MotorRun(motor_rechts, 120)
+robotbit.MotorRun(motor_links, 120)
